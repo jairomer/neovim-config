76b2279f62ea9705b1166b4368ae6ee44833f5dd
synk <jairomer@protonmail.com>
2024-05-05 18:54:15 +0200

    Now we have automatic LSP setup with Mason.

--- init.lua
+++ init.lua
@@ -81,7 +81,21 @@ require('mason').setup({
     }
 })
 
+-- We want this to automate LSP installation.
 require("mason-lspconfig").setup()
+require("mason-lspconfig").setup_handlers {
+    -- The first entry (without a key) will be the default handler
+    -- and will be called for each installed server that doesn't have
+    -- a dedicated handler.
+    function (server_name) -- default handler (optional)
+        require("lspconfig")[server_name].setup {}
+    end,
+    -- Next, you can provide a dedicated handler for specific servers.
+    -- For example, a handler override for the `rust_analyzer`:
+    --["rust_analyzer"] = function ()
+    --    require("rust-tools").setup {}
+    --end
+}
 
 -- BarBar -----------------------------------------
 local map = vim.api.nvim_set_keymap
